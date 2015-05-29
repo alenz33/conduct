afea13ecc6327b4092e4b3a47c60b2da15761dda
Alexander Lenz <alexander.lenz@frm2.tum.de>
2015-05-29 11:43:16 +0200

    Move cli param parsing to own module.

--- conduct.py
+++ conduct.py
@@ -34,83 +34,9 @@ import conduct
 
 from conduct import loggers
 from conduct.chain import Chain
-from conduct.util import loadChainDefinition, loadChainConfig, \
-    chainPathToName, loadConductConf, analyzeSystem
+from conduct.cli import parseArgv
+from conduct.util import loadChainDefinition, loadChainConfig, analyzeSystem
 
-def processGlobalArgs(parser, argv):
-    '''
-    Parse 'global' arguments that does not belong to any chain.
-    '''
-    parser.add_argument('-g',
-                        '--global-config',
-                        type=str,
-                        help='Global config file (conduct.conf)',)
-
-    parser.add_argument('-c',
-                        '--chain',
-                        type=chainPathToName,
-                        help='Desired chain',
-                        required=True)
-
-    parser.add_argument('-h',
-                        '--help',
-                        help='Print help',
-                        action='store_true')
-
-    # parse global args
-    globalArgs, specArgs = parser.parse_known_args(argv)
-
-    # init config
-    # load and store global config
-    conduct.cfg = loadConductConf(globalArgs.global_config)
-
-    # handle help stuff
-    if globalArgs.help and not specArgs:
-        parser.print_help()
-        print('')
-        print('Available subcommands: build')
-        parser.exit()
-
-    return globalArgs
-
-def addChainArgs(parser, chainName):
-    '''
-    Add parameters of given chain (by name) as arguments to the argparse parser.
-    '''
-    chainDef = loadChainDefinition(chainName)
-
-    for paramName, paramDef in chainDef['parameters'].items():
-        flag = '--%s' % paramName
-        parser.add_argument(
-            flag,
-            type=paramDef.type,
-            help=paramDef.description,
-            #required=(paramDef.default == None), # may be part of param file
-        )
-
-
-
-def parseArgv(argv):
-    '''
-    Parse command line arguments.
-    '''
-    parser = argparse.ArgumentParser(description='conduct - CONvenient Construction Tool',
-                                     conflict_handler='resolve',
-                                     add_help=False)
-
-    globalArgs = processGlobalArgs(parser, argv)
-
-    subparsers = parser.add_subparsers(title='actions',
-                                       description='valid actions',
-                                       dest='action')
-
-    build = subparsers.add_parser('build', help='Build chain')
-
-    # add chain specific params
-    if globalArgs.chain:
-        addChainArgs(build, globalArgs.chain)
-
-    return parser.parse_args(argv)
 
 def initLogging(daemonize=False):
     '''
